0d4aaa5e2815a30e21f672ec6994350b148dadc6
amara <sanohawa@gmail.com>
2019-12-17 00:28:55 +0100

    Composant search : Select Diet et cuisine.

--- src/components/Search.js
+++ src/components/Search.js
@@ -8,34 +8,54 @@ import {CuisineData} from '../../data/CuisineData';
 const Search = () => {
   const [diet, setDiet] = useState("");
   const [cuisine, setCuisine] = useState("");
-  console.log(CuisineData); 
-  
+
+
+  const pickerStyle = {
+    inputIOS: {
+      color: 'orange',
+      paddingTop: 13,
+      paddingHorizontal: 10,
+      paddingBottom: 12
+    },
+    inputAndroid: {
+      color: 'orange'
+    }
+  };
+
   const selectDiet = (DietData) => {
     return (
       <RNPickerSelect
             onValueChange={(value) => setDiet(value)}
-            style={styles.selectPicker}
-            placeholder={ {label: 'Diet ?', value: null} }
             items={DietData}
+            placeholder={ {label: 'Diet ?', value: null, color: 'red'} }
+            style={pickerStyle}
         />
     ); 
-  }
+  }; 
 
   const selectCuisine = (CuisineData) => {
     return (
       <RNPickerSelect
             onValueChange={(value) => setCuisine(value)}
-            style={styles.selectPicker}
-            placeholder={ {label: 'Cuisine ?', value: null} }
             items={CuisineData}
+            placeholder={ {label: 'Cuisine ?', value: null, color: 'red'} }
+            style={pickerStyle}
         />
     ); 
-  }
+  }; 
+
+  const selectsDietAndCuisine = (DietData, CuisineData) => {
+    return (
+      <View style={{flexDirection: 'row'}}>
+        <View style={{flex:1}}>{selectDiet(DietData)}</View>
+        <View style={{flex:1}}>{selectCuisine(CuisineData)}</View>
+      </View>
+    ); 
+  };
 
   return (
     <View style={styles.container}>
-        {selectDiet(DietData)}
-        {selectCuisine(CuisineData)}
+      {selectsDietAndCuisine(DietData, CuisineData)}
     </View>
   );
 }
@@ -46,11 +66,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }, 
-  selectPicker: {
-    flex:1,
-    backgroundColor: 'purple'
+    marginTop: 15
   }
 });
