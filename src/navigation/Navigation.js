fccc33249fc4283e3c4795b6bd495b15790c527c
amara <sanohawa@gmail.com>
2019-12-19 00:59:15 +0100

    Mise en place de la tab navigation.

---
+++ src/navigation/Navigation.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { StyleSheet, Image } from 'react-native';
+
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+
+import Search from '../components/Search'; 
+import Me from '../components/Me';
+import Setting from '../components/Setting';
+
+const TabNavigation = createBottomTabNavigator(
+    {
+      Search: {
+        screen: Search,
+        navigationOptions: {
+          title: 'Search',
+          headerTitle: 'Search',
+          tabBarIcon: () => {
+            return <Image style={ styles.tabIcon } source={{ uri: 'image' }}/>
+          },
+        },
+      },
+      Me: {
+        screen: Me,
+        navigationOptions: {
+          title: 'Me',
+          tabBarIcon: () => {
+            return <Image style={ styles.tabIcon } source={{ uri: 'image' }}/>
+          },
+        },
+      },
+      Setting: {
+        screen: Setting,
+        navigationOptions: {
+          title: 'Setting',
+          tabBarIcon: () => {
+            return <Image style={ styles.tabIcon } source={{ uri: 'image' }}/>
+          },
+        },
+      },
+    }, 
+    {
+      tabBarOptions: {
+        activeBackgroundColor: 'orange',
+        activeTintColor: 'white',
+      },
+      initialRouteName: 'Me',
+    }
+  );
+  
+  export default createAppContainer(TabNavigation);
+
+  const styles = StyleSheet.create({
+    tabIcon: {
+      width: 20,
+      height: 20,
+      backgroundColor: 'white'
+    },
+  });
