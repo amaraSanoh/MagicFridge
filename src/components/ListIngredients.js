7f40d721148369fbfb301d01d6d51a089111ec58
amaraSanoh <sanohawa@gmail.com>
2020-02-16 14:27:18 +0100

    correctif what can i cook today et correctif ajout d'ingredients dans le frigo et dans la liste

--- src/components/ListIngredients.js
+++ src/components/ListIngredients.js
@@ -4,14 +4,14 @@ import { colors } from '../../definitions/Colors';
 import Ingredient from './Ingredient';
 
 
-const ListIngredients = ({ingredients, refreshTop, refreshing, ingredientsExtras, isFrigo, isList}) => {
+const ListIngredients = ({ingredients, refreshTop, refreshing, ingredientsExtras, isFrigo, isList, isAddToFridge, isAddToList}) => {
 
     return (
         <FlatList
             data={ingredients}
             extraData={ ingredientsExtras }
             keyExtractor={(item) => item.id.toString() }
-            renderItem={ ({item}) => <Ingredient isFrigo={isFrigo} isList={isList} ingredient={item} /> }
+            renderItem={ ({item}) => <Ingredient isFrigo={isFrigo} isList={isList} isAddToFridge={isAddToFridge} isAddToList={isAddToList} ingredient={item} /> }
             onRefresh={ refreshTop }  //Permet de faire le chargement une fois en haut de la liste
             refreshing={ refreshing } //chargement si isRefreshing est à true
         />
